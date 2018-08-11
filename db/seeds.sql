d252577ed4d62ef5dd3cb788544ab84090561c98
Laxman Chamlagai <laxmanchamlagai20@gmail.com>
2018-08-11 14:19:29 -0400

    added start script

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Seed Burgers into Database ...
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("Fish sandwich", false, "2018-08-07 01:43:00"),
+  ("Bacon burger", false, "2018-08-07 01:47:02"),
+  ("Vegburger", false, "2018-08-07 01:50:00"),
+  ("totol grill", false, "2018-08-07 01:53:20"),
+  ("Honeysweet chicken", true, "2018-08-07 01:55:16");
